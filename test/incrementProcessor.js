b55a902faddbb7ef19b71b559aa90707de79c521
Tim Park <timfpark@gmail.com>
2017-12-20 08:26:50 -0800

    emit -> output

--- test/incrementProcessor.js
+++ test/incrementProcessor.js
@@ -2,8 +2,10 @@ const { Processor } = require('../lib');
 
 class IncrementProcessor extends Processor {
     process(message, callback) {
+        if (Math.random() < 0.5) return callback("fake error");
+
         message.number += 1;
-        this.emit([message], callback);
+        this.output([message], callback);
     }
 }
 
