9e71e9a9316182cc9bdc5aab9108952214e25ce5
Tim Park <timfpark@gmail.com>
2018-01-12 11:42:07 -0800

    add Log to index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Connection: require('./lib/connection'),
+    Log: require('./lib/log'),
     Node: require('./lib/node'),
     Processor: require('./lib/processor'),
     Topology: require('./lib/topology')
